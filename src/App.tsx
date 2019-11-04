d51e234c62da5a8c2af79399feb0e706656c5434
TheLacrox <antonio.fran.sanchez.hernandez@gmail.com>
2019-11-04 19:36:06 +0000

    Deleted unused Css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className="w-full">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
